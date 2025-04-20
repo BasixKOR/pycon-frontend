abfcdba58ee0ba26aa227b20aba23181bece5ace
MUsoftware <musoftware@daum.net>
2025-04-20 16:12:12 +0900

    feat: 추가한 디렉토리를 패키지로 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+interface ViteTypeOptions {
+  strictImportEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_PYCONKR_SHOP_API_DOMAIN: string;
+  readonly VITE_PYCONKR_SHOP_CSRF_COOKIE_NAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/frontend/",
+  envDir: "./dotenv",
   plugins: [react(), mdx()],
+  resolve: {
+    alias: {
+      '@pyconkr-common': path.resolve(__dirname, './package/pyconkr-common'),
+      '@pyconkr-shop': path.resolve(__dirname, './package/pyconkr-shop'),
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
 });
