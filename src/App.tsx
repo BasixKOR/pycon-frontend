3a3a3705f4a6fdb90d7a1aae865700f061168161
y00eunji <aine0227@naver.com>
2025-04-30 09:02:49 +0900

    feat: MUI 테마 및 글로벌 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import Test from "./components/Test";
+import { Global } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { muiTheme, globalStyles } from "./styles/globalStyles";
+
+import Header from "./components/layout/Header";
 
 function App() {
-  return <Test />;
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Global styles={globalStyles} />
+      <Header />
+    </ThemeProvider>
+  );
 }
 
 export default App;
