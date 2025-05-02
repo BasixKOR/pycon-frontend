ad3afcf7d49f58a72cc3d8415abc025cf9abf625
y00eunji <aine0227@naver.com>
2025-05-02 10:03:06 +0900

    feat: 헤더 메뉴 동작 추가

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,6 +1,32 @@
 import styled from "@emotion/styled";
+import { useMenu } from "../../../hooks/useMenu";
 
-export default function Header() {
+interface SubMenuItem {
+  text: string;
+  href: string;
+}
+
+interface MenuItem {
+  text: string;
+  href?: string;
+  subMenu?: SubMenuItem[];
+}
+
+interface HeaderProps {
+  menus: MenuItem[];
+}
+
+export default function Header({ menus }: HeaderProps) {
+  const {
+    hoveredMenu,
+    focusedMenu,
+    menuRefs,
+    setHoveredMenu,
+    setFocusedMenu,
+    handleKeyDown,
+    handleBlur,
+  } = useMenu();
+
   return (
     <HeaderContainer>
       <HeaderLogo>
@@ -14,12 +40,34 @@ export default function Header() {
 
       <nav>
         <HeaderNav>
-          <li>파이콘 한국</li>
-          <li>프로그램</li>
-          <li>세션</li>
-          <li>구매</li>
-          <li>재정 지원</li>
-          <li>후원하기</li>
+          {menus.map((menu) => (
+            <li
+              key={menu.text}
+              ref={(el) => {
+                menuRefs.current[menu.text] = el;
+              }}
+              onMouseEnter={() => setHoveredMenu(menu.text)}
+              onMouseLeave={() => setHoveredMenu(null)}
+              onFocus={() => setFocusedMenu(menu.text)}
+              onBlur={() => handleBlur(menu)}
+              onKeyDown={(e) => handleKeyDown(e, menu)}
+              tabIndex={0}
+            >
+              {menu.text}
+              {menu.subMenu &&
+                (hoveredMenu === menu.text || focusedMenu === menu.text) && (
+                  <SubMenu>
+                    {menu.subMenu.map((subItem) => (
+                      <SubMenuItem key={subItem.text}>
+                        <a href={subItem.href} tabIndex={0}>
+                          {subItem.text}
+                        </a>
+                      </SubMenuItem>
+                    ))}
+                  </SubMenu>
+                )}
+            </li>
+          ))}
         </HeaderNav>
       </nav>
       <HeaderLeft>
@@ -50,6 +98,7 @@ const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: relative;
 `;
 
 const HeaderLogo = styled.div`
@@ -64,6 +113,18 @@ const HeaderNav = styled.ul`
   gap: 2rem;
   font-size: 0.875rem;
   font-weight: 500;
+  position: relative;
+
+  li {
+    position: relative;
+    cursor: pointer;
+    outline: none;
+
+    &:focus {
+      outline: 2px solid ${({ theme }) => theme.palette.primary.main};
+      outline-offset: 1px;
+    }
+  }
 `;
 
 const HeaderLeft = styled.div`
@@ -76,4 +137,41 @@ const HeaderItem = styled.div`
   display: flex;
   align-items: center;
   gap: 0.625rem;
+`;
+
+const SubMenu = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: white;
+  border-radius: 5px;
+  padding: 5px 0;
+  width: 125px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+`;
+
+const SubMenuItem = styled.li`
+  padding: 5px 0;
+  text-align: center;
+
+  a {
+    color: ${({ theme }) => theme.palette.primary.light};
+    text-decoration: none;
+    font-size: 10px;
+    display: block;
+    outline: none;
+
+    &:hover,
+    &:focus {
+      color: ${({ theme }) => theme.palette.primary.main};
+      font-weight: 600;
+    }
+
+    &:focus {
+      outline: 2px solid ${({ theme }) => theme.palette.primary.main};
+      outline-offset: 0.5px;
+    }
+  }
 `;
