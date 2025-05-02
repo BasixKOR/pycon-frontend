1fa5fa47a447ea05c69467848d8b620be939a5ff
y00eunji <aine0227@naver.com>
2025-05-02 10:13:48 +0900

    feat: MUI 테마 및 글로벌 스타일 적용으로 App 구성 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
-import { Global } from "@emotion/react";
-import { muiTheme, globalStyles } from "./styles/globalStyles";
 import MainLayout from "./components/layout";
 import Test from "./components/Test";
 
 function App() {
   return (
-    <ThemeProvider theme={muiTheme}>
-      <CssBaseline />
-      <Global styles={globalStyles} />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Test />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Test />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
