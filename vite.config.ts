d292ddf9035d929a25207146df08c7941c5b742c
MUsoftware <musoftware@daum.net>
2025-05-12 09:16:29 +0900

    add: ShopAPI 도메인 정의 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
 import path from 'path';
 import { defineConfig } from "vite";
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/frontend/",
   envDir: "./dotenv",
-  plugins: [react(), mdx()],
+  plugins: [react(), mdx(), mkcert({ hosts: ["local.dev.pycon.kr"] })],
   resolve: {
     alias: {
       '@pyconkr-common': path.resolve(__dirname, './package/pyconkr-common'),
