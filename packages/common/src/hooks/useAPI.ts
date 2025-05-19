7e71a966ec8a1eca9cd9e039d5096fefea5751bd
MUsoftware <musoftware@daum.net>
2025-05-19 09:52:25 +0900

    chore: 불필요한 hook 제거

--- packages/common/src/hooks/useAPI.ts
+++ packages/common/src/hooks/useAPI.ts
@@ -5,12 +5,10 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import BackendAPIs from "../apis";
 import { BackendAPIClient } from '../apis/client';
 import BackendContext from '../contexts';
-import Utils from "../utils";
 
 const QUERY_KEYS = {
   SITEMAP: ["query", "sitemap"],
   SITEMAP_LIST: ["query", "sitemap", "list"],
-  SITEMAP_NESTED_LIST: ["query", "sitemap", "list", "nested"],
   PAGE: ["query", "page"],
 };
 
@@ -30,15 +28,6 @@ namespace BackendAPIHooks {
     queryKey: QUERY_KEYS.SITEMAP_LIST,
     queryFn: clientDecorator(BackendAPIs.listSiteMaps),
     meta: { invalidates: [ QUERY_KEYS.SITEMAP ] },
-  });
-
-  export const useNestedSiteMapQuery = () => useSuspenseQuery({
-    queryKey: QUERY_KEYS.SITEMAP_LIST,
-    queryFn: async () => {
-      const { data } = useFlattenSiteMapQuery();
-      return Utils.buildNestedSiteMap(data);
-    },
-    meta: { invalidates: [ QUERY_KEYS.SITEMAP_NESTED_LIST ] },
   });
 
   export const usePageQuery = (id: string) => useSuspenseQuery({
