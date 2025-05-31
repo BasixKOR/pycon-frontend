d4760bd7effde6c74fa3bfa23a3a9bc304f77979
MUsoftware <musoftware@daum.net>
2025-06-01 01:57:09 +0900

    chore: 로컬에서 스테이징 서버로 admin의 개발이 가능하도록 mkcert 설정 추가

--- apps/pyconkr-admin/vite.config.ts
+++ apps/pyconkr-admin/vite.config.ts
@@ -3,13 +3,14 @@ import path from "path";
 import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   envDir: "../../dotenv",
-  plugins: [react(), mdx(), svgr()],
+  plugins: [react(), mdx(), mkcert({ hosts: ["local.dev.pycon.kr"] }), svgr()],
   resolve: {
     alias: {
       "@frontend/common": path.resolve(
