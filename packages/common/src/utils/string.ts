b014d64fe42747f33b5549fcf43a245475b3dfad
MUsoftware <musoftware@daum.net>
2025-06-03 13:26:58 +0900

    feat: isValidHttpUrl helper 함수 추가

--- packages/common/src/utils/string.ts
+++ packages/common/src/utils/string.ts
@@ -2,5 +2,15 @@ import * as R from "remeda";
 
 export const isFilledString = (obj: unknown): obj is string => R.isString(obj) && !R.isEmpty(obj);
 
+export const isValidHttpUrl = (obj: unknown): obj is string => {
+  try {
+    const url = new URL(obj as string);
+    return url.protocol === "http:" || url.protocol === "https:";
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    return false;
+  }
+};
+
 // Remove whitespace from the right side of the input string.
 export const rtrim = (x: string): string => x.replace(/\s+$/gm, "");
