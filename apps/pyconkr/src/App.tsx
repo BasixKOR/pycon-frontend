5a6e580f76e135df19aed64e6f31ef61c0f29af2
MUsoftware <musoftware@daum.net>
2025-06-06 23:05:18 +0900

    chore: AppContext 추가

--- apps/pyconkr/src/App.tsx
+++ apps/pyconkr/src/App.tsx
@@ -1,36 +1,48 @@
 import * as Common from "@frontend/common";
-import React from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import * as React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import * as R from "remeda";
 
-import MainLayout from "./components/layout";
-import { Test } from "./components/pages/test";
-import { IS_DEBUG_ENV } from "./consts/index.ts";
-import { SponsorProvider } from "./contexts/SponsorContext";
-
-// 스폰서를 표시할 페이지 경로 설정
-const SPONSOR_VISIBLE_PATHS = ["/"];
-
-const AppContent = () => {
-  const location = useLocation();
-  const shouldShowSponsor = SPONSOR_VISIBLE_PATHS.includes(location.pathname);
-
-  return (
-    <SponsorProvider initialVisibility={shouldShowSponsor}>
-      <Routes>
-        <Route element={<MainLayout />}>
-          {IS_DEBUG_ENV && <Route path="/debug" element={<Test />} />}
-          <Route path="/pages/:id" element={<Common.Components.PageIdParamRenderer />} />
-          <Route path="*" element={<Common.Components.RouteRenderer />} />
-        </Route>
-      </Routes>
-    </SponsorProvider>
-  );
-};
+import MainLayout from "./components/layout/index.tsx";
+import { PageIdParamRenderer, RouteRenderer } from "./components/pages/dynamic_route.tsx";
+import { Test } from "./components/pages/test.tsx";
+import { IS_DEBUG_ENV } from "./consts";
+import { useAppContext } from "./contexts/app_context";
+import BackendAPISchemas from "../../../packages/common/src/schemas/backendAPI";
 
 export const App: React.FC = () => {
+  const backendAPIClient = Common.Hooks.BackendAPI.useBackendClient();
+  const { data: flatSiteMap } = Common.Hooks.BackendAPI.useFlattenSiteMapQuery(backendAPIClient);
+  const siteMapNode = Common.Utils.buildNestedSiteMap(flatSiteMap)?.[""];
+
+  const location = useLocation();
+  const { setAppContext, language } = useAppContext();
+
+  React.useEffect(() => {
+    (async () => {
+      const currentRouteCodes = ["", ...location.pathname.split("/").filter((code) => !R.isEmpty(code))];
+      const currentSiteMapDepth: (BackendAPISchemas.NestedSiteMapSchema | undefined)[] = [siteMapNode];
+
+      for (const routeCode of currentRouteCodes.splice(1)) {
+        currentSiteMapDepth.push(currentSiteMapDepth.at(-1)?.children[routeCode]);
+        if (R.isNullish(currentSiteMapDepth.at(-1))) {
+          console.warn(`Route not found in site map: ${routeCode}`);
+          break;
+        }
+      }
+
+      setAppContext((ps) => ({ ...ps, siteMapNode, currentSiteMapDepth }));
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location, language, flatSiteMap]);
+
   return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
+    <Routes>
+      <Route element={<MainLayout />}>
+        {IS_DEBUG_ENV && <Route path="/debug" element={<Test />} />}
+        <Route path="/pages/:id" element={<PageIdParamRenderer />} />
+        <Route path="*" element={<RouteRenderer />} />
+      </Route>
+    </Routes>
   );
 };
