7c82e4883aea7f7196a68bba80ed4b689741335e
MUsoftware <musoftware@daum.net>
2025-06-12 08:31:48 +0900

    fix: 법에 맞도록 footer 수정

--- apps/pyconkr/src/components/layout/Footer/index.tsx
+++ apps/pyconkr/src/components/layout/Footer/index.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import * as Common from "@frontend/common";
-import { Article, Email, Facebook, GitHub, Instagram, LinkedIn, X, YouTube } from "@mui/icons-material";
+import { Article, Email, Facebook, GitHub, Instagram, LinkedIn, OpenInNew, X, YouTube } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import * as React from "react";
 
 import FlickrIcon from "@apps/pyconkr/assets/thirdparty/flickr.svg?react";
@@ -18,7 +19,6 @@ interface IconItem {
 
 interface FooterProps {
   slogan?: string;
-  description?: string;
   links?: LinkItem[];
   icons?: IconItem[];
 }
@@ -54,13 +54,16 @@ const defaultIcons: IconItem[] = [
   },
 ];
 
+const Bar: React.FC = () => <div style={{ display: "inline-block", padding: "0 0.25rem" }}>|</div>;
+
 export default function Footer({
-  slogan = "Weave with Python, 파이콘 한국 2025",
-  description = "파이콘 한국 2025는 파이콘 한국 준비위원회가 만들고 있습니다\n파이썬 웹 프레임워크 Django로 만들었습니다",
   links = [
-    { text: "파이콘 한국 행동 강령(CoC)", href: "#" },
-    { text: "서비스 이용 약관", href: "#" },
-    { text: "개인 정보 처리 방침", href: "#" },
+    {
+      text: "파이콘 한국 행동 강령(CoC)",
+      href: "https://pythonkr.github.io/pycon-code-of-conduct/ko/coc/a_intent_and_purpose.html",
+    },
+    { text: "서비스 이용 약관", href: "/about/terms-of-service" },
+    { text: "개인 정보 처리 방침", href: "/about/privacy-policy" },
   ],
   icons = defaultIcons,
 }: FooterProps) {
@@ -69,10 +72,28 @@ export default function Footer({
   return (
     <FooterContainer>
       <FooterContent>
-        <FooterSlogan>{slogan}</FooterSlogan>
-        {description.split("\n").map((line, index) => (
-          <div key={index}>{line}</div>
-        ))}
+        <FooterText>
+          <strong>사단법인 파이썬사용자모임</strong>
+          <br />
+          서울특별시 강남구 강남대로84길 24-4
+          <Bar />
+          대표자명 : 배권한
+          <Bar />
+          대표 전화 번호 : 031-261-2203, 010-5298-6622, 010-8259-3013 (문자)
+          <Bar />
+          사업자 등록 번호 : 338-82-00046
+          <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=3388200046" target="_blank" rel="noreferrer">
+            <Button variant="outlined" startIcon={<OpenInNew sx={{ fontSize: "7pt" }} />}>
+              사업자 정보 확인
+            </Button>
+          </a>
+          <br />
+          통신 판매 번호 : 2023-서울강남-03501
+          <Bar />
+          호스팅 제공자 : Amazon Web Services(Korea LLC)
+          <Bar />
+          문의: <a href="mailto:pyconkr@pycon.kr">pyconkr@pycon.kr</a>
+        </FooterText>
         <FooterLinks>
           {links.map((link, index) => (
             <React.Fragment key={index}>
@@ -99,6 +120,7 @@ export default function Footer({
             </IconLink>
           ))}
         </FooterIcons>
+        <FooterSlogan>© 2025, 파이콘 한국 준비위원회, All rights reserved.</FooterSlogan>
       </FooterContent>
     </FooterContainer>
   );
@@ -113,7 +135,8 @@ const FooterContainer = styled.footer`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 267px;
+  max-height: 16rem;
+  padding: 1rem 0;
 `;
 
 const FooterContent = styled.div`
@@ -124,8 +147,38 @@ const FooterContent = styled.div`
   gap: 0.75rem;
 `;
 
+const FooterText = styled.div`
+  padding: 0 2rem;
+  margin: 0.1rem;
+
+  font-size: 9pt;
+
+  a > button {
+    margin-left: 0.25rem;
+    padding: 0.05rem 0.25rem;
+    font-size: 8pt;
+    color: ${({ theme }) => theme.palette.common.white};
+    border-color: ${({ theme }) => theme.palette.common.white};
+
+    gap: 0.25rem;
+
+    & span {
+      margin-left: -2px;
+      margin-right: 0;
+
+      & svg {
+        font-size: 12pt !important;
+      }
+    }
+  }
+
+  strong {
+    font-size: 12pt;
+  }
+`;
+
 const FooterSlogan = styled.div`
-  font-weight: 600;
+  text-align: center;
 `;
 
 const FooterLinks = styled.div`
