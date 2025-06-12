172a969c93721ca84341a93531ea429a17d2fae1
MUsoftware <musoftware@daum.net>
2025-06-12 13:26:34 +0900

    feat: Footer 영문 번역 추가

--- apps/pyconkr/src/components/layout/Footer/index.tsx
+++ apps/pyconkr/src/components/layout/Footer/index.tsx
@@ -6,21 +6,12 @@ import * as React from "react";
 
 import FlickrIcon from "@apps/pyconkr/assets/thirdparty/flickr.svg?react";
 
-interface LinkItem {
-  text: string;
-  href: string;
-}
+import { useAppContext } from "../../../contexts/app_context";
 
 interface IconItem {
   icon: React.FC<{ width?: number; height?: number }>;
   alt: string;
   href: string;
-}
-
-interface FooterProps {
-  slogan?: string;
-  links?: LinkItem[];
-  icons?: IconItem[];
 }
 
 const defaultIcons: IconItem[] = [
@@ -56,43 +47,77 @@ const defaultIcons: IconItem[] = [
 
 const Bar: React.FC = () => <div style={{ display: "inline-block", padding: "0 0.25rem" }}>|</div>;
 
-export default function Footer({
-  links = [
+export default function Footer() {
+  const { sendEmail } = Common.Hooks.Common.useEmail();
+  const { language } = useAppContext();
+
+  const corpPasamoStr = language === "ko" ? "사단법인 파이썬사용자모임" : "Python Korea";
+  const corpAddressStr =
+    language === "ko"
+      ? "서울특별시 강남구 강남대로84길 24-4"
+      : "24-4, Gangnam-daero 84-gil, Gangnam-gu, Seoul, Republic of Korea";
+  const corpRepresentatorStr = language === "ko" ? "대표자명 : 배권한" : "Representator : Kwon-Han Bae";
+  const corpPhoneStr =
+    language === "ko"
+      ? "대표 전화 번호 : 031-261-2203, 010-5298-6622, 010-8259-3013 (문자)"
+      : "Phone Number : 031-261-2203, 010-5298-6622, 010-8259-3013 (SMS)";
+  const corpCompanyNumberStr =
+    language === "ko" ? "사업자 등록 번호 : 338-82-00046" : "Business Registration Number : 338-82-00046";
+  const corpCheckBtnStr = language === "ko" ? "사업자 정보 확인" : "Check Business Registration Information";
+  const corpMailOrderSalesRegistrationNumberStr =
+    language === "ko"
+      ? "통신 판매 번호 : 2023-서울강남-03501"
+      : "Mail Order Sales Registration Number : 2023-SEOUL-GANGNAM-03501";
+  const hostingProviderStr =
+    language === "ko"
+      ? "호스팅 제공자 : Amazon Web Services(Korea LLC)"
+      : "Hosting Provider : Amazon Web Services(Korea LLC)";
+  const contractEmailStr = language === "ko" ? "문의: " : "Contact: ";
+  const copyrightStr =
+    language === "ko"
+      ? "© 2025, 사단법인 파이썬사용자모임, All rights reserved."
+      : "© 2025, Python Korea, All rights reserved.";
+
+  const links = [
     {
-      text: "파이콘 한국 행동 강령(CoC)",
+      text: language === "ko" ? "파이콘 한국 행동 강령(CoC)" : "PyCon Korea Code of Conduct",
       href: "https://pythonkr.github.io/pycon-code-of-conduct/ko/coc/a_intent_and_purpose.html",
     },
-    { text: "서비스 이용 약관", href: "/about/terms-of-service" },
-    { text: "개인 정보 처리 방침", href: "/about/privacy-policy" },
-  ],
-  icons = defaultIcons,
-}: FooterProps) {
-  const { sendEmail } = Common.Hooks.Common.useEmail();
+    {
+      text: language === "ko" ? "서비스 이용 약관" : "Terms of Service",
+      href: "/about/terms-of-service",
+    },
+    {
+      text: language === "ko" ? "개인 정보 처리 방침" : "Privacy Policy",
+      href: "/about/privacy-policy",
+    },
+  ];
 
   return (
     <FooterContainer>
       <FooterContent>
         <FooterText>
-          <strong>사단법인 파이썬사용자모임</strong>
+          <strong>{corpPasamoStr}</strong>
           <br />
-          서울특별시 강남구 강남대로84길 24-4
+          {corpAddressStr}
           <Bar />
-          대표자명 : 배권한
+          {corpRepresentatorStr}
           <Bar />
-          대표 전화 번호 : 031-261-2203, 010-5298-6622, 010-8259-3013 (문자)
+          {corpPhoneStr}
           <Bar />
-          사업자 등록 번호 : 338-82-00046
+          {corpCompanyNumberStr}
           <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=3388200046" target="_blank" rel="noreferrer">
             <Button variant="outlined" startIcon={<OpenInNew sx={{ fontSize: "7pt" }} />}>
-              사업자 정보 확인
+              {corpCheckBtnStr}
             </Button>
           </a>
           <br />
-          통신 판매 번호 : 2023-서울강남-03501
+          {corpMailOrderSalesRegistrationNumberStr}
           <Bar />
-          호스팅 제공자 : Amazon Web Services(Korea LLC)
+          {hostingProviderStr}
           <Bar />
-          문의: <a href="mailto:pyconkr@pycon.kr">pyconkr@pycon.kr</a>
+          {contractEmailStr}
+          <a href="mailto:pyconkr@pycon.kr">pyconkr@pycon.kr</a>
         </FooterText>
         <FooterLinks>
           {links.map((link, index) => (
@@ -108,7 +133,7 @@ export default function Footer({
           <IconLink onClick={sendEmail} aria-label="이메일 보내기">
             <Email width={20} height={20} aria-hidden="true" />
           </IconLink>
-          {icons.map((icon) => (
+          {defaultIcons.map((icon) => (
             <IconLink
               key={icon.alt}
               href={icon.href}
@@ -120,7 +145,7 @@ export default function Footer({
             </IconLink>
           ))}
         </FooterIcons>
-        <FooterSlogan>© 2025, 파이콘 한국 준비위원회, All rights reserved.</FooterSlogan>
+        <FooterSlogan>{copyrightStr}</FooterSlogan>
       </FooterContent>
     </FooterContainer>
   );
