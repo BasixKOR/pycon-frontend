29486ed993bde4ab4d6aa9d7b635246263f702ab
MUsoftware <musoftware@daum.net>
2025-06-14 17:39:43 +0900

    fix: FallbackImg 컴포넌트 내부로 props가 전달되지 않는 문제 수정

--- packages/common/src/components/fallback_image.tsx
+++ packages/common/src/components/fallback_image.tsx
@@ -4,7 +4,7 @@ type FallbackImageProps = React.ComponentProps<"img"> & {
   errorFallback: React.ReactNode;
 };
 
-export const FallbackImage: React.FC<FallbackImageProps> = ({ src, alt, errorFallback }) => {
+export const FallbackImage: React.FC<FallbackImageProps> = ({ errorFallback, src, alt, ...props }) => {
   const [isError, setIsError] = React.useState(!src ? true : false);
-  return isError ? errorFallback : <img src={src} alt={alt} onError={() => setIsError(true)} />;
+  return isError ? errorFallback : <img src={src} alt={alt} {...props} onError={() => setIsError(true)} />;
 };
