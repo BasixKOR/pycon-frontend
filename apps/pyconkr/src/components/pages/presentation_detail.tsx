1457cb531491569858a571f6dd3e303a3bc95748
MUsoftware <musoftware@daum.net>
2025-07-13 21:23:32 +0900

    fix: 발표 자료 URL은 가리도록 수정

--- apps/pyconkr/src/components/pages/presentation_detail.tsx
+++ apps/pyconkr/src/components/pages/presentation_detail.tsx
@@ -144,7 +144,7 @@ export const PresentationDetailPage: React.FC = ErrorBoundary.with(
     const descriptionFallback = language === "ko" ? "해당 발표의 설명은 준비 중이에요!" : "Description of the presentation is under preparation!";
     const categoriesStr = language === "ko" ? "카테고리" : "Categories";
     const speakersStr = language === "ko" ? "발표자" : "Speakers";
-    const slideShowStr = language === "ko" ? "발표 슬라이드" : "Presentation Slideshow";
+    // const slideShowStr = language === "ko" ? "발표 슬라이드" : "Presentation Slideshow";
 
     React.useEffect(() => {
       setAppContext((prev) => ({
@@ -175,7 +175,7 @@ export const PresentationDetailPage: React.FC = ErrorBoundary.with(
             <Divider flexItem />
           </>
         ) : null}
-        {presentation.slideshow_url && (
+        {/* {presentation.slideshow_url && (
           <>
             <Typography variant="subtitle1" fontWeight="bold" sx={{ width: "100%", p: 2, a: { color: "blue" } }}>
               {slideShowStr} :&nbsp;
@@ -183,7 +183,7 @@ export const PresentationDetailPage: React.FC = ErrorBoundary.with(
             </Typography>
             <Divider flexItem />
           </>
-        )}
+        )} */}
         {presentation.image && (
           <StyledPresentationImage
             alt="Presentation Image"
